bb3bee0143ff1a4c01738527cad5015b1577ae65
Gorynychdo <gorynychdo@gmail.com>
2020-04-02 12:28:19 +0300

    Record confirmed is default

--- internal/app/store/recordrepository.go
+++ internal/app/store/recordrepository.go
@@ -9,8 +9,8 @@ type RecordRepository struct {
 func (r *RecordRepository) Create(rec *model.Record) error {
     return r.store.db.QueryRow(
         `INSERT INTO records
-            (account_id, wav_file_name, creation_time, complete_time, confirmed)
-        VALUES ((SELECT id from users WHERE tel = ?), ?, ?, ?, 3)`,
+            (account_id, wav_file_name, creation_time, complete_time)
+        VALUES ((SELECT id from users WHERE tel = ?), ?, ?, ?)`,
         rec.Endpoint,
         rec.FileName,
         rec.Created,
